1b2c65eebea5b90e4ce02b27a194a4ff76da7e2d
Dicebot <public@dicebot.lv>
2015-07-11 14:39:13 +0300

    Use vibe.http.auth instead of custom solution

--- source/mood/main.d
+++ source/mood/main.d
@@ -32,44 +32,18 @@ void main(string[] args)
 
     auto router = new URLRouter;
     // protected sections require authorization first before route fall-through
-    router.get("/testauth", &basic_auth);
-    router.post(MoodURLConfig.apiBase ~ MoodURLConfig.posts, &basic_auth);
+    import vibe.http.auth.basic_auth;
+    auto auth_dg = performBasicAuth(
+        MoodAuthConfig.realm,
+        (user, pwd) => user == MoodAuthConfig.user && pwd == MoodAuthConfig.password
+    );
+    router.post(MoodURLConfig.apiBase ~ MoodURLConfig.posts, auth_dg);
 
     // "real" request handlers
     router.registerRestInterface(app.API());
-    router.get(MoodURLConfig.posts, &app.postHTML);
-    router.get("*", serveStaticFiles(Path(MoodPathConfig.statics)));
+    router.get (MoodURLConfig.posts, &app.postHTML);
+    router.get ("*", serveStaticFiles(Path(MoodPathConfig.statics)));
 
     listenHTTP(settings, router);
     runEventLoop();
-}
-
-void basic_auth(HTTPServerRequest req, HTTPServerResponse res)
-{
-    import std.base64;
-    import std.format : format;
-    import std.exception : enforce;
-
-    res.headers["WWW-Authenticate"] = "Basic realm=\"" ~ MoodAuthConfig.realm ~ "\"";
-
-    if (!req.ssl)
-    {
-        auto url = req.fullURL();
-        url.schema = "https";
-        res.redirect(url);
-        return;
-    }
-
-    auto auth = req.headers.getAll("Auhtorization");
-    if (auth.length != 1)
-        throw new HTTPStatusException(HTTPStatus.unauthorized);
-
-    auto expected = format(
-        "Basic %s",
-        Base64.encode(cast(ubyte[]) format(
-            "%s:%s", MoodAuthConfig.user, MoodAuthConfig.password))
-    );
-
-    if (auth[0] != expected)
-        throw new HTTPStatusException(HTTPStatus.unauthorized);
 }
